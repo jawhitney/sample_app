ba18fa03a1e2a44e808357ccc9f89c199548f21c
Jonathan Whitney <jonathan.whitney@keynote.org>
2011-12-08 11:52:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '049aeafe476673b87a0a7855d6d8289cd79372edabcd68efd2e4520e95b728b90e2c2d8e5b55852342318926cb4d3a00bb8b5f8dbb7f92e9b479eccb6ca4c8e9'
